b0bd4a69009d3ef98e3fccf667820494b59551e3
tejashvini10 <74137391+tejashvini10@users.noreply.github.com>
2021-08-10 17:39:21 +0530

    Add files via upload

---
+++ PRO-C33-master/box.js
@@ -0,0 +1,28 @@
+class Box {
+    constructor(x, y, width, height) {
+      var options = {
+        isStatic:false,
+          'restitution':0,
+          'friction':1.0,
+          'density':1.0
+        
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      
+      push();
+      //translate(pos.x, pos.y);
+      //rotate(angle);
+      rectMode(CENTER);
+      fill(225)
+   
+      rect(pos.x,pos.y, this.width, this.height);
+      pop();
+    }
+  };
